50d65089631d1e72a96adf0a85edb741fe305339
Hyeminah <nolwenn.dewasch@hotmail.com>
2023-09-20 15:25:13 +0200

    initial origin

---
+++ main.ts
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const canvas = document.querySelector<HTMLCanvasElement>("#particules-canvas")!;
+
+const ctx = canvas.getContext("2d")!;
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+ctx.fillStyle = "#000000";
+ctx.fillRect(0, 0, width, height);
+
+ctx.fillStyle = "#ff0000";
+ ctx.arc(width / 100, height / 45, 10, 0, Math.PI * 2);
+ ctx.fill();
