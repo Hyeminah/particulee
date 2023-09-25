28604e0da439b9d01442bb16a75f0dd5cebb27a2
Hyeminah <nolwenn.dewasch@hotmail.com>
2023-09-25 10:09:31 +0200

    step 3 completed

--- main.ts
+++ main.ts
@@ -3,26 +3,12 @@ const canvas = document.querySelector<HTMLCanvasElement>("#particules-canvas")!;
 const ctx = canvas.getContext("2d")!;
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
-
-
 ctx.fillStyle = "#000000";
 ctx.fillRect(0, 0, width, height);
 
-const dotcolor = "#ff0000";
-ctx.fillStyle = dotcolor ;
-const circleRadius = 50; // Adjust the size of the circle here
- const  numberofdot = 4
-
-function makedot (x, y) {
-    ctx.fillStyle = dotcolor;
-    ctx.beginPath();
-    ctx.arc(x, y, circleRadius, 0, Math.PI * 2);
-    ctx.fill();
-}
-
-for ( let i =0; i < numberofdot; i++ ) {
-    makedot(circleRadius, circleRadius);
-    makedot(canvas.width - circleRadius, circleRadius);
-     makedot(circleRadius, canvas.height - circleRadius);
-     makedot(canvas.width - circleRadius, canvas.height - circleRadius);
-}
+ctx.fillStyle = "#ff0000";
+const circleRadius = 20; // Adjust the size of the circle here
+const circleX = 20 ; // Position the circle on the left
+const circleY = 20 ; // Position the circle at the top
+ctx.arc(circleX, circleY, circleRadius, 0, Math.PI * 2);
+ctx.fill();
